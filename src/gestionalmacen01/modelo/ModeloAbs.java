bdb48db5fa70e764ce3eda7d965d8d35d505ff6c
gonzsanz <davidgonzsanz02@gmail.com>
2022-05-01 14:51:35 +0200

    He implementado el modelo de HashMap

--- src/gestionalmacen01/modelo/ModeloAbs.java
+++ src/gestionalmacen01/modelo/ModeloAbs.java
@@ -4,23 +4,20 @@ package gestionalmacen01.modelo;
  * Abstract class ModeloAbs - Clase de acceso a Modelo de DATOS
  * 
  * @author: Alberto Lopez
- * Date: 24/04/2018
+ *          Date: 24/04/2018
  */
-public interface  ModeloAbs
-{
-    
-    public boolean insertarProducto ( Producto p);
-    
-    boolean borrarProducto ( int codigo );
-    
-    public Producto buscarProducto ( int codigo);
-    
-    void listarProductosTodos ();
-    
+public interface ModeloAbs {
+
+    public boolean insertarProducto(Producto p);
+
+    boolean borrarProducto(int codigo);
+
+    public Producto buscarProducto(int codigo);
+
+    void listarProductosTodos();
+
     void listarProductosStockMin();
-    
-    
-    boolean modificarProducto (Producto nuevo);
-    
-    
+
+    boolean modificarProducto(Producto nuevo);
+
 }
--- src/gestionalmacen01/modelo/ModeloHashMap.java
+++ src/gestionalmacen01/modelo/ModeloHashMap.java
@@ -18,4 +18,53 @@ public class ModeloHashMap implements ModeloAbs {
         lista = new HashMap<Integer, Producto>();
     }
 
+    public boolean insertarProducto(Producto p) {
+
+        lista.put(p.getCodigo(), p);
+        return true;
+    }
+
+    public boolean borrarProducto(int codigo) {
+
+        if (lista.get(codigo) != null) {
+            lista.remove(codigo);
+            return true;
+        }
+        return false;
+    }
+
+    public Producto buscarProducto(int codigo) {
+        if (lista.get(codigo) != null) {
+            return lista.get(codigo);
+        }
+        System.out.println("No existe el producto con codigo: " + codigo);
+        return null;
+    }
+
+    public void listarProductosTodos() {
+
+        for (Map.Entry<Integer, Producto> entry : lista.entrySet()) {
+
+            System.out.println(entry.getValue().toString());
+        }
+    }
+
+    public void listarProductosStockMin() {
+        for (Map.Entry<Integer, Producto> entry : lista.entrySet()) {
+            Producto valor = entry.getValue();
+            if (valor.getStock() < valor.getStock_min()) {
+
+                System.out.println(valor);
+            }
+        }
+    }
+
+    public boolean modificarProducto(Producto nuevo) {
+
+        if (lista.get(nuevo.getCodigo()) != null) {
+            lista.replace(nuevo.getCodigo(), nuevo);
+            return true;
+        }
+        return false;
+    }
 }
